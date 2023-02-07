42fc9ae16a0c4495d6579bbd4c590a1d462b0425
Carla Amat <carla.amatpinna@gmail.com>
2023-02-08 00:04:18 +0100

    Modif back-end

--- src/custom-errors/http-error.helper.js
+++ src/custom-errors/http-error.helper.js
@@ -2,10 +2,10 @@ const httpErrorHelper = (err, req, res, _next) => {
   console.error(err.stack);
   switch (err.name) {
     case "NotFoundError":
-      return res.status(404).send(err.message);
+      return res.status(404).send({err: err.message});
     case "ValidationError":
     case "DuplicateError":
-      return res.status(400).send(err.message);
+      return res.status(400).send({err: err.message});
     default:
       return res.status(500).send("Something broke!");
   }
